c0b4078539a39d5dc001ab4b61ebb5fc4f839f63
Abubakar Athman <athman.abu@gmail.com>
2022-05-21 13:17:32 +0300

    without toggle night mode

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Hero, About } from "./sections/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <About />
+    </div>
+  );
+}
+
+export default App;
